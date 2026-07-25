5386e694ebd798e99a41e23abd9527aad630531d
Samala Aryan <samalaaryan123@gmail.com>
2026-07-25 18:49:16 +0530

    feat: design URL model and apply initial migrations

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Shortener(models.Model):
+    url = models.URLField(max_length=200)
+    short_code = models.CharField(max_length=8, unique=True)
+    access_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.short_code
