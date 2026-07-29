00ca52808ce79211d7c5cd606a02101663ac6e9a
Samala Aryan <samalaaryan123@gmail.com>
2026-07-29 17:15:58 +0530

    Add retrieve endpoint with access count tracking

--- shortener/api/urls.py
+++ shortener/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ShortenerCreateView
+from .views import ShortenerCreateView, ShortenerRetrieveView
 
 urlpatterns = [
     path('shorten/', ShortenerCreateView.as_view(), name='shorten'),
+    path('shorten/<str:short_code>/', ShortenerRetrieveView.as_view(), name='retrieve'),
 ]
--- shortener/api/views.py
+++ shortener/api/views.py
@@ -6,3 +6,17 @@ from .serializers import ShortenerSerializer
 class ShortenerCreateView(generics.CreateAPIView):
     queryset = Shortener.objects.all()
     serializer_class = ShortenerSerializer
+
+
+class ShortenerRetrieveView(generics.RetrieveAPIView):
+    queryset = Shortener.objects.all()
+    serializer_class = ShortenerSerializer
+    lookup_field = "short_code"
+
+    def get_object(self):
+        obj = super().get_object()
+
+        obj.access_count += 1
+        obj.save()
+
+        return obj
