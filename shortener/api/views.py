7b655f29b7ed7a4de8a1fc7f2289fc0704777a99
Samala Aryan <samalaaryan123@gmail.com>
2026-07-30 12:12:36 +0530

    Add update and delete support using RetrieveUpdateDestroyAPIView

--- shortener/api/views.py
+++ shortener/api/views.py
@@ -10,7 +10,7 @@ class ShortenerCreateView(generics.CreateAPIView):
     serializer_class = ShortenerSerializer
 
 
-class ShortenerDetailView(generics.RetrieveUpdateAPIView):
+class ShortenerDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Shortener.objects.all()
     serializer_class = ShortenerSerializer
     lookup_field = "short_code"
