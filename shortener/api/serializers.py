816ec23cb1a672a7f756dc0d642e664ebe79a805
Samala Aryan <samalaaryan123@gmail.com>
2026-07-31 15:56:41 +0530

    Add support for custom short codes

--- shortener/api/serializers.py
+++ shortener/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from shortener.models import Shortener
 
 class ShortenerSerializer(serializers.ModelSerializer):
-
+    short_code = serializers.CharField(required = False, max_length = 8)
     class Meta:
         model = Shortener
         fields = '__all__'
@@ -13,3 +13,20 @@ class ShortenerSerializer(serializers.ModelSerializer):
                             'created_at', 
                             'updated_at', 
                             )
+        
+    def validate_short_code(self, value):
+
+        if self.instance:
+            if value != self.instance.short_code:
+                raise serializers.ValidationError(
+                    "Short code cannot be changed after creation."
+                )
+
+            return value
+
+        if Shortener.objects.filter(short_code=value).exists():
+            raise serializers.ValidationError(
+                "This short code is already taken."
+            )
+
+        return value
