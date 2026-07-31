20a87290d1dc6a00ef907da11126d4f769037e94
Samala Aryan <samalaaryan123@gmail.com>
2026-07-31 16:17:30 +0530

    Add QR code generation for shortened URLs

--- shortener/api/urls.py
+++ shortener/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from shortener.views import QRCodeView
 from .views import ShortenerCreateView, ShortenerDetailView, ShortenerStatsView
 
 urlpatterns = [
     path('shorten/', ShortenerCreateView.as_view(), name='shorten'),
     path('shorten/<str:short_code>/stats/', ShortenerStatsView.as_view(), name='stats'),
+    path("shorten/<str:short_code>/qr/", QRCodeView.as_view(), name="qr-code"),
     path('shorten/<str:short_code>/', ShortenerDetailView.as_view(), name='retrieve'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,6 @@
+import qrcode
+
+from io import BytesIO
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.http import HttpResponse
@@ -23,3 +26,25 @@ class RedirectView(View):
         obj.save(update_fields=["access_count"])
 
         return redirect(obj.url)
+
+
+class QRCodeView(View):
+    def get(self, request, short_code):
+        obj = get_object_or_404(
+            Shortener,
+            short_code=short_code
+        )
+
+        short_url = request.build_absolute_uri(
+            f"/{obj.short_code}/"
+        )
+
+        qr = qrcode.make(short_url)
+
+        buffer = BytesIO()
+        qr.save(buffer, format="PNG")
+
+        return HttpResponse(
+            buffer.getvalue(),
+            content_type="image/png"
+        )
