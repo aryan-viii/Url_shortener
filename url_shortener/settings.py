d2d720df4e5977570d8259459256886027b61462
Samala Aryan <samalaaryan123@gmail.com>
2026-07-31 16:35:21 +0530

    Add API rate limiting with DRF throttling

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -120,5 +120,13 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 10,
+
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+    },
 }
